80df0baa3fc95e33df17c8a7d159e211508b3398
betty <betty.sujin.lee@gmail.com>
2025-07-24 17:01:10 +0900

    [init] 새 프로젝트 생성

---
+++ SiKKuRo/SiKKuRoTests/SiKKuRoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SiKKuRoTests.swift
+//  SiKKuRoTests
+//
+//  Created by betty on 7/24/25.
+//
+
+import Testing
+@testable import SiKKuRo
+
+struct SiKKuRoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
